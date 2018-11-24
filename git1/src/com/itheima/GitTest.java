40acb8ded787e979a675a718c0d3bb8e272a9104
yx <yangxv15@163.com>
2018-11-24 19:21:46 +0800

    第一次提交

---
+++ git1/src/com/itheima/GitTest.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+/**
+ * @author mxs
+ * @date 2018/11/24 0024 19:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("nihao ");
+        System.out.println("nihao ");
+        System.out.println("nihao ");
+        System.out.println("nihao ");
+    }
+}
